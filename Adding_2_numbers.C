0f1fab6aed8f088df9a34b78afd244bffc741403
Prashant-jain-02 <113042902+Prashant-jain-02@users.noreply.github.com>
2022-09-12 21:08:33 +0530

    Create Adding_2_numbers.C

---
+++ Adding_2_numbers.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+
+{
+
+int a,b,c;
+printf("Enter First number :");
+scanf("%d",&a);
+printf("Enter second number :");
+scanf("%d",&b);
+c = a+b;
+printf("sum of values %d and %d was %d",a,b,c);
+
+}
